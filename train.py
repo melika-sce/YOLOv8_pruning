7e883b1ba4b43575a73d842ebcd4cdec2ea3de35
melika.sce <melika.sce@gmail.com>
2024-01-05 13:29:16 +0330

    debug

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
-results = model.train(data='VisDrone.yaml', epochs=1, imgsz=1024, batch=1, bn_sparsity=False, ft_pruned_model=False, Prune=True)
+results = model.train(data='VisDrone.yaml', epochs=1, imgsz=1024, batch=1, bn_sparsity=False, ft_pruned_model=False, prune=True)
 
